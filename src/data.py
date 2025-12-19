48710a94ebbf12ea799b81f8af04cf6adb29f055
LovreJurjevic <lovre881@gmail.com>
2025-12-19 11:44:13 +0100

    add src module

---
+++ src/data.py
@@ -0,0 +1,45 @@
+import json
+
+
+PATH_TO_JSON_FILE = "../fastener_dataset/annotations/instances_default.json"
+PATH_TO_IMAGES_FOLDER = "../fastener_dataset/images"
+
+def extract_data():
+    with open(PATH_TO_JSON_FILE, "r") as f:
+        data = json.load(f)
+
+    images = data["images"]
+    print("Number of images: ",len(images))
+    print("Attributes: ",list(images[0].keys()))
+    print()
+
+    annotations = data["annotations"]
+    print("Number of annotations (they refer to images):",len(annotations))
+    print("Attributes: ", list(annotations[0].keys()))
+    print()
+    last_annotation = annotations[-1]
+    print("IDs don't always match!")
+    print(last_annotation["id"],"!=",last_annotation["image_id"])
+    paths = []
+    labels = []
+    labels_for_subclasses = []
+
+    for annotation in annotations:
+        image_id = annotation["image_id"]
+
+        file_name = ""
+
+        #sram me ove petlje, vjerovatno se moze nekako aproksimirat uz pomoc image_id-a
+        for image in images:
+            if str(image["id"]) == str(image_id):
+                file_name = image["file_name"]
+                break
+
+        #u jsonu se nalaze krivi nazivi slika pa je potrebno ispraviti
+        paths.append(PATH_TO_IMAGES_FOLDER + "/" + file_name.replace(":","_"))
+        labels.append(annotation["category_id"])
+        #labels_for_subclasses.append(annotation["category"])
+
+    print("Size of paths and labels should be the same: ", len(paths), len(labels), len(labels_for_subclasses))
+    
+    return paths, labels
---
+++ src/dataset.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from sklearn.model_selection import train_test_split
+import data
+
+data_augmentation = tf.keras.Sequential([
+        layers.RandomFlip("horizontal"),
+        layers.RandomFlip("vertical"),
+        layers.RandomRotation(0.2),
+        layers.RandomZoom(0.2),
+    ])
+
+def load_and_preprocess_image(path, label):
+    img = tf.io.read_file(path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    #img = tf.image.rgb_to_grayscale(img)
+    img = tf.image.resize(img, (512,512))
+    img = tf.cast(img, tf.float32) / 255.0
+    return img, label
+
+def create_dataset(paths, labels, is_for_training = False):
+    ds = tf.data.Dataset.from_tensor_slices((paths, labels))
+    ds = ds.map(load_and_preprocess_image, num_parallel_calls=tf.data.AUTOTUNE)
+    if is_for_training:
+        ds = ds.shuffle(buffer_size=1000)
+        ds = ds.map(
+            lambda x, y: (data_augmentation(x, training=True), y),
+            num_parallel_calls=tf.data.AUTOTUNE
+        )
+    ds = ds.batch(32).prefetch(tf.data.AUTOTUNE)
+    return ds
+
+
+def create_datasets():
+    paths, labels = data.extract_data()
+
+    train_val_paths, test_paths, train_val_labels, test_labels = train_test_split(
+        paths, 
+        labels, 
+        test_size=0.2, 
+        random_state=42, 
+        shuffle=True,
+        stratify=labels
+    )
+
+    train_paths, val_paths, train_labels, val_labels = train_test_split(
+        train_val_paths, 
+        train_val_labels, 
+        test_size=0.25, 
+        random_state=42, 
+        shuffle=True,
+        stratify=train_val_labels
+    )
+
+    print(f"Training samples: {len(train_paths)}")
+    print(f"Validation samples: {len(val_paths)}")
+    print(f"Testing samples: {len(test_paths)}")
+
+    
+    train_ds = create_dataset(train_paths, train_labels, is_for_training=True)
+    val_ds = create_dataset(val_paths, val_labels)
+    test_ds = create_dataset(test_paths, test_labels)
+
+    return train_ds, val_ds, test_ds
